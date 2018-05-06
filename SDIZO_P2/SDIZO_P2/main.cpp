6ff7e4b9ac03f0078248f66e589c425d9ee6bc24
krzysiekmwk <krzysiek.mwk@gmail.com>
2018-05-06 17:54:59 +0200

    Tworzenie losowego grafu spojnego

--- SDIZO_P2/SDIZO_P2/main.cpp
+++ SDIZO_P2/SDIZO_P2/main.cpp
@@ -15,17 +15,19 @@ void fillGraphMatrix(GraphMatrix *&gm);
 
 void randData(int countOfVertexs, int density, GraphMatrix *&gm);
 
+bool directed = false; // Czy graf ma byc skierowany czy nie
+
 int main() {
 	srand(time(NULL));
 	GraphMatrix *gm;
 
 	fillGraphMatrix(gm);
 
-	randData(5, 99, gm);
-
-	//displayGraphMatrixAsMatrix(*&gm);
+	randData(5, 50, gm);
+	cout << "\n\n";
+	displayGraphMatrixAsMatrix(*&gm);
 	cout << "\n\n\n\n\n";
-	//displayGraphMatrixAsList(*&gm);
+	displayGraphMatrixAsList(*&gm);
 	cout << "\n\n\n\n\n";
 
 	int x;
@@ -74,7 +76,7 @@ void fillGraphMatrix(GraphMatrix *&gm) {
 
 		//cout << edges << "\t" << vertex << endl;
 
-		gm = new GraphMatrix(atoi(vertex.c_str()), false);
+		gm = new GraphMatrix(atoi(vertex.c_str()), directed);
 
 		for (int i = 0; i < atoi(edges.c_str()); i++) {
 			string line;
@@ -99,15 +101,62 @@ void fillGraphMatrix(GraphMatrix *&gm) {
 
 void randData(int countOfVertexs, int density, GraphMatrix *&gm) {
 	int countOfEdges = (((countOfVertexs * (countOfVertexs - 1)) / 2) * density) / 100;
-	gm = new GraphMatrix(countOfVertexs, false);
+	int minCountOfEdges = countOfVertexs - 1;
 
+	if (countOfEdges < minCountOfEdges)	//Jesli dla podanej objetosci graf nie mozne bys spojny, to nalezy sprawic by byl (zwiekszyc ilosc krawedzi do minimum)
+		countOfEdges = minCountOfEdges;
+	countOfEdges = countOfEdges - minCountOfEdges; //Ilosc polaczen pozostala do wylosowania, po tym jak graf juz bedzie spojny
+
+	gm = new GraphMatrix(countOfVertexs, directed);
+
+	/*****Najpierw tworzymy minimalny graf spojny dla N-1********/
+	int *vertexTab = new int[countOfVertexs];	//Tablica przechowujaca ile wierzcholkow ma juz jakies polaczenie z czyms
+	for (int i = 0; i < countOfVertexs; i++)
+		vertexTab[i] = 0;
+
+	//Losowanie peirwszego polaczenia dwoch losowych wierzcholkow
+	int randV1;
+	int randV2;
+	int randWeight = rand() % 100 + 1;
+
+	do {
+		randV1 = rand() % countOfVertexs;
+		randV2 = rand() % countOfVertexs;
+	} while (randV1 == randV2);
+
+	vertexTab[randV1] = 1;
+	vertexTab[randV2] = 1;
+	gm->insert(randV1, randV2, randWeight);
+	minCountOfEdges--;
+
+	//Losowanie reszty wierzcholkow, polaczonych do tych co juz sa - wynikiem bedzie powstanie graf spojny
+	while (minCountOfEdges > 0) {
+
+		do {
+			randV1 = rand() % countOfVertexs;
+		} while (vertexTab[randV1] == 1);	//Szukanie wierzcholka ktory nie jest jeszcze z zadnym polaczony
+
+		do {
+			randV2 = rand() % countOfVertexs;
+		} while (vertexTab[randV2] != 1);	//Szukanie wierzcholka ktory ma juz jakies polaczenie
+
+		randWeight = rand() % 100 + 1;
+		gm->insert(randV1, randV2, randWeight);
+		vertexTab[randV1] = 1;
+		minCountOfEdges--;
+	}
+
+	delete[] vertexTab; // Tablica nie bedzie juz dalej potrzebna
+
+	/*****Graf jest juz spojny, teraz trzeba dolosowac polaczenia, tak aby gestosc grafu byla odpowiednia********/
+	
 	while (countOfEdges > 0) {
-		int randV1 = rand() % countOfVertexs;
-		int randV2 = rand() % countOfVertexs;
+		randV1 = rand() % countOfVertexs;
+		randV2 = rand() % countOfVertexs;
 
 		if (randV1 != randV2) {
 			if (gm->search(randV1, randV2) == 0 && gm->search(randV2, randV1) == 0) {
-				int randWeight = rand() % 100 + 1;
+				randWeight = rand() % 100 + 1;
 				gm->insert(randV1, randV2, randWeight);
 				countOfEdges--;
 			}
